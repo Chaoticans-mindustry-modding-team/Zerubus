332d8dd7dafb4700d3d393c7b184a7599c8cccce
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-02-13 07:01:18 -0700

    Create ZerubusJavaMod.java

---
+++ src/zerubus/ZerubusJavaMod.java
@@ -0,0 +1,41 @@
+package javacamod;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+import javacamod.content.*;
+
+public class ZerubusJavaMod extends Mod{
+
+    public ZerubusJavaMod(){
+        Log.info("Loaded ZerubusJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("Zerubus");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("The pink tides and red grass calls for your return.", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        ZerubusBlocks.load();
+        ZerubusItems.load();
+        ZerubusLiquids.load();
+        Log.info("Loadin");
+    }
+
+}
