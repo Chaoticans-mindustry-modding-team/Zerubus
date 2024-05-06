9fe4c64b69c28ef475f65e617460a78f08c867c6
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-05-05 19:27:48 -0600

    Update ZerubusItems.java

--- src/zerubus/content/ZerubusItems.java
+++ src/zerubus/content/ZerubusItems.java
@@ -6,10 +6,18 @@ import mindustry.type.*;
 
 public class ZerubusItems{
     public static Item
-    
+    indium, silver
     public static void load(){
       indium = new Item("indium", Color.valueOf("000080")){{
           hardness = 1;
           cost = 0.5f;
           alwaysUnlocked = true;
       }};
+
+      silver = new Item("silver", Color.valueOf("a0a0ac")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+  
