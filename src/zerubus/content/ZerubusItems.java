d25009eda03364539540bc0f7ad538d67de20a8d
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-05-05 19:23:48 -0600

    Create ZerubusItems.java

---
+++ src/zerubus/content/ZerubusItems.java
@@ -0,0 +1,15 @@
+package zerubus.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class ZerubusItems{
+    public static Item
+    
+    public static void load(){
+      indium = new Item("indium", Color.valueOf("000080")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
