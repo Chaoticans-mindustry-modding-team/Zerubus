0abee7986a53717ad54bb7576fc2a88e85661569
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-05-05 19:45:41 -0600

    Update ZerubusJavaMod.java

--- src/zerubus/ZerubusJavaMod.java
+++ src/zerubus/ZerubusJavaMod.java
@@ -33,8 +33,8 @@ public class ZerubusJavaMod extends Mod{
     @Override
     public void loadContent(){
         Log.info("Loadin");
-        JavacamodBlocks.load();
-        JavacamodItems.load();
+        ZerubusBlocks.load();
+        ZerubusItems.load();
     }
 
 }
