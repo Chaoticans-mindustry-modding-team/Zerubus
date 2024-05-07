91fc250cc83b914baf1ed267812c27d685aeb840
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-05-07 01:22:45 -0600

    Create ZerubusLiquids.java

---
+++ src/zerubus/content/ZerubusLiquids.java
@@ -0,0 +1,18 @@
+package zerubus.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class ZerubusLiquids{
+    public static Liquid
+    
+
+    public static void load(){
+
+        oxygen = new Liquid("oxygen", Color.valueOf("00aa99")){{
+            gas = true;
+            barColor = Color.valueOf("00aa99");
+            explosiveness = 1f;
+            flammability = 1f;
+        }};
