a6a2b5a13d6a1a69a06b2fa056203a1ec8540f44
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-05-09 21:05:16 -0600

    Update ZerubusPlanets.java

--- src/zerubus/content/ZerubusPlanets.java
+++ src/zerubus/content/ZerubusPlanets.java
@@ -41,12 +41,12 @@ public class ZerubusPlanets{
                 Color.valueOf("ff8546"),
                 Color.valueOf("f49878")
             );
-            orbitSpacing = 5f;
+            orbitSpacing = 100f;
         }};
 
         zerubus = new Planet("zerubus", cataclysm, 1f, 2){{
             generator = new ZerubusPlanetGenerator();
-            meshLoader = () -> new HexMesh(this, 5);
+            meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("aa0000").a(0.75f), 2, 0.42f, 1f, 0.43f),
                 new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("aa00aa").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
